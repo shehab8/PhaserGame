0399b58ccd5df65c50ddcff4a9d24591debf86d2
Shehab <sheh0050@stud.kea.dk>
2020-07-14 20:55:37 +0200

    second commit

--- public/js/MainScene.js
+++ public/js/MainScene.js
@@ -1,2 +1,15 @@
-class MainScene extends Phaser.Scene {
+import Snake from './Snake.js'
+
+export default class MainScene extends Phaser.Scene {
+  constructor(){
+      super('MainScene');
+  }
+
+  create(){
+      this.snake = new Snake(this);
+  }
+
+  update(time) {
+     this.snake.update(time);
+  }
 }
