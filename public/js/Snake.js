7bd419368e60a0220bef0de6f7bc6eb2397aa734
Shehab <sheh0050@stud.kea.dk>
2020-07-15 13:47:35 +0200

    apple placement

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -1,43 +1,62 @@
 export default class Snake {
     constructor(scene) {
         this.scene = scene;
+        this.lastMovetime = 0;
+        this.moveInterval = 300;
+        this.tileSize = 16;
         this.direction = Phaser.Math.Vector2.DOWN;
         this.body = [];
         this.body.push(
-            this.scene.add.rectangle(100,100,16,16, 0xff0000).setOrigin(0)
+            this.scene.add.rectangle(this.scene.game.config.width / 2, this.scene.game.config.height / 2,
+                this.tileSize, this.tileSize, 0xff0000).setOrigin(0)
         );
-        this.body.push(
-            this.scene.add.rectangle(0,0,16,16, 0x0000ff).setOrigin(0)
-        );
+        this.apple = this.scene.add.rectangle(0, 0, this.tileSize, this.tileSize, 0x00ff00)
+            .setOrigin(0);
+
+        this.positionApple();
 
         scene.input.keyboard.on('keydown', e => {
             this.keydown(e);
         });
     }
 
-    keydown(event){
+    positionApple() {
+        this.apple.x = Math.floor(
+            (Math.random() * this.scene.game.config.width) / this.tileSize) * this.tileSize;
+        this.apple.y = Math.floor(
+            (Math.random() * this.scene.game.config.height) / this.tileSize) * this.tileSize;
+    }
+
+    keydown(event) {
         console.log(event);
         switch (event.keyCode) {
             case 37: // left
-            this.direction = Phaser.Math.Vector2.LEFT;
-             break;
+                this.direction = Phaser.Math.Vector2.LEFT;
+                break;
             case 39: // right
-            this.direction = Phaser.Math.Vector2.RIGHT;
-             break;
+                this.direction = Phaser.Math.Vector2.RIGHT;
+                break;
             case 38: // up
-            this.direction = Phaser.Math.Vector2.UP;
-             break;
+                this.direction = Phaser.Math.Vector2.UP;
+                break;
             case 40: // down
-            this.direction = Phaser.Math.Vector2.DOWN;
-             break; 
+                this.direction = Phaser.Math.Vector2.DOWN;
+                break;
         }
     }
 
     update(time) {
-        this.body[0].x += this.direction.x;
-        this.body[0].y += this.direction.y;
-        
-
+        if (time >= this.lastMovetime + this.moveInterval) {
+            this.lastMovetime = time;
+            this.move();
+        }
     }
-
+    move() {
+        for (let index = this.body.length - 1; index > 0; index--) {
+            this.body[index].x = this.body[index - 1].x;
+            this.body[index].y = this.body[index - 1].y;
+        }
+        this.body[0].x += this.direction.x * 16;
+        this.body[0].y += this.direction.y * 16;
+    }
 }
