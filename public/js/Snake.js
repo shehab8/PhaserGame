3a754a8af50ace0cc2154cb4fe15e6700b0aa076
Shehab <sheh0050@stud.kea.dk>
2020-07-15 15:26:29 +0200

    Snake movement

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -2,77 +2,17 @@ export default class Snake {
     constructor(scene) {
         this.scene = scene;
         this.lastMovetime = 0;
-        this.moveInterval = 150;
+        this.moveInterval = 100;
         this.tileSize = 16;
         this.direction = Phaser.Math.Vector2.DOWN;
         this.body = [];
         this.body.push(
             this.scene.add.rectangle(
-                this.scene.game.config.width / 2, 
+                this.scene.game.config.width / 2,
                 this.scene.game.config.height / 2,
-                this.tileSize, 
-                this.tileSize, 
+                this.tileSize,
+                this.tileSize,
                 0xff0000)
-                .setOrigin(0)
-        );
-
-        this.body.push(
-            this.scene.add.rectangle(
-                this.scene.game.config.width / 2, 
-                this.scene.game.config.height / 2,
-                this.tileSize, 
-                this.tileSize, 
-                0xffffff)
-                .setOrigin(0)
-        );
-
-        this.body.push(
-            this.scene.add.rectangle(
-                this.scene.game.config.width / 2, 
-                this.scene.game.config.height / 2,
-                this.tileSize, 
-                this.tileSize, 
-                0xffffff)
-                .setOrigin(0)
-        );
-
-        this.body.push(
-            this.scene.add.rectangle(
-                this.scene.game.config.width / 2, 
-                this.scene.game.config.height / 2,
-                this.tileSize, 
-                this.tileSize, 
-                0xffffff)
-                .setOrigin(0)
-        );
-
-        this.body.push(
-            this.scene.add.rectangle(
-                this.scene.game.config.width / 2, 
-                this.scene.game.config.height / 2,
-                this.tileSize, 
-                this.tileSize, 
-                0xffffff)
-                .setOrigin(0)
-        );
-
-        this.body.push(
-            this.scene.add.rectangle(
-                this.scene.game.config.width / 2, 
-                this.scene.game.config.height / 2,
-                this.tileSize, 
-                this.tileSize, 
-                0xffffff)
-                .setOrigin(0)
-        );
-
-        this.body.push(
-            this.scene.add.rectangle(
-                this.scene.game.config.width / 2, 
-                this.scene.game.config.height / 2,
-                this.tileSize, 
-                this.tileSize, 
-                0xffffff)
                 .setOrigin(0)
         );
 
@@ -97,16 +37,20 @@ export default class Snake {
         console.log(event);
         switch (event.keyCode) {
             case 37: // left
-                this.direction = Phaser.Math.Vector2.LEFT;
+                if (this.direction !== Phaser.Math.Vector2.RIGHT)
+                    this.direction = Phaser.Math.Vector2.LEFT;
                 break;
             case 39: // right
-                this.direction = Phaser.Math.Vector2.RIGHT;
+                if (this.direction !== Phaser.Math.Vector2.LEFT)
+                    this.direction = Phaser.Math.Vector2.RIGHT;
                 break;
             case 38: // up
-                this.direction = Phaser.Math.Vector2.UP;
+                if (this.direction !== Phaser.Math.Vector2.DOWN)
+                    this.direction = Phaser.Math.Vector2.UP;
                 break;
             case 40: // down
-                this.direction = Phaser.Math.Vector2.DOWN;
+                if (this.direction !== Phaser.Math.Vector2.UP)
+                    this.direction = Phaser.Math.Vector2.DOWN;
                 break;
         }
     }
